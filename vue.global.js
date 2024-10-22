6e38563c50c91327374b27c73e15aec4a69867d7
Yang Kang Yan <kyyang.2023@scis.smu.edu.sg>
2024-10-23 00:24:05 +0800

    Create vue.global.js

---
+++ vue.global.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({ 
+            data() { 
+                return { 
+
+                };
+            }, 
+            methods: {
+
+                },
+
+            } // methods
+        );
+        const vm = app.mount('#app'); 
